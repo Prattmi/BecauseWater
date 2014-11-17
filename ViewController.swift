12ffc6fd6509a5971b65db58057e502c9ff66d63
Prattmi <prattmi@bu.edu>
2014-11-16 21:10:57 -0500

    Location Update
    This commit mainly pertains to being able to update the users current
    location and setting up a reverse geocode to pre fill text boxes later.

--- ViewController.swift
+++ ViewController.swift
@@ -8,8 +8,79 @@
 
 import UIKit
 
-class ViewController: UIViewController/*, TypesTableViewControllerDelegate*/ {
+class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDelegate {
+
+    @IBOutlet weak var mapView: GMSMapView!
+    //@IBOutlet weak var mapCenterPinImage: UIImageView!
+    @IBOutlet weak var pinImageVerticalConstraint: NSLayoutConstraint!
+    var searchedTypes = ["bakery", "bar", "cafe", "grocery_or_supermarket", "restaurant"]
+        //2
+    let locationManager = CLLocationManager()
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            //3
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        //Makes the mapView Controller the maps views delegate
+        mapView.delegate = self
+        }
+
+    // 1
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        // 2
+        if status == .AuthorizedWhenInUse {
+            
+            // 3
+            locationManager.startUpdatingLocation()
+            
+            //4
+            mapView.myLocationEnabled = true
+            mapView.settings.myLocationButton = true
+        }
+    }
     
+    // 5
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        if let location = locations.first as? CLLocation {
+            
+            // 6 Ask for optimal startup zoom
+            mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 17, bearing: 0, viewingAngle: 0)
+            
+            // This tells the location manager to stop updating the users location, we dont need this
+            //locationManager.stopUpdatingLocation()
+        }
+    }
+    
+    //Needed to get the users current location
+    func reverseGeocodeCoordinate(coordinate: CLLocationCoordinate2D) {
+        
+        // 1
+        let geocoder = GMSGeocoder()
+        //Moves update location button up above bottom toolbar
+        let labelHeight: CGFloat = 50
+        self.mapView.padding = UIEdgeInsets(top: self.topLayoutGuide.length, left: 0, bottom: labelHeight, right: 0)
+        
+        // 2
+        geocoder.reverseGeocodeCoordinate(coordinate) { response , error in
+            if let address = response?.firstResult() {
+                
+                // 3
+                let lines = address.lines as [String]
+                /*self.addressLabel.text = join("\n", lines)
+                
+                // 4
+                UIView.animateWithDuration(0.25) {
+                    self.view.layoutIfNeeded()
+*/
+                }
+            }
+        }
+    
+    //Call on this function when the map stops moving so you can make a reverse geocall
+    func mapView(mapView: GMSMapView!, idleAtCameraPosition position: GMSCameraPosition!) {
+        reverseGeocodeCoordinate(position.target)
+    }
     //@IBOutlet weak var mapView: GMSMapView!
     
     //The below code was taken out because it has to do with the pin object
@@ -18,11 +89,11 @@ class ViewController: UIViewController/*, TypesTableViewControllerDelegate*/ {
     var searchedTypes = ["bakery", "bar", "cafe", "grocery_or_supermarket", "restaurant"]
     */
     
-    override func viewDidLoad() {
+    /*override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
+    */
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
